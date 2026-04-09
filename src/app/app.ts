e76482c8d420bc9d46291821193574acde784da3
EnzoPss <enzopousse.06@gmail.com>
2026-04-09 15:57:05 +0200

    transmition pages ancien projet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Profil } from "./components/profil/profil";
+import { Parcours } from "./components/parcours/parcours";
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Profil, Parcours, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrls: ['./header.css']
 })
-export class Header {
+export class Header implements OnInit {
+  isScrolled = false;
 
+  // --- Personnalisation ---
+  profileImage = 'https://www.photofunky.net/output/image/b/a/0/9/ba0992/photofunky.gif'; // Remplace par le chemin de ton image
+  firstName = 'Kyliann';
+  lastName = 'MEDERIC '/*Nanette'*/;
+  tagline = 'Commercial';
+  // ------------------------
+
+  ngOnInit(): void {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+    this.isScrolled = scrollY > 40;
+  }
 }
